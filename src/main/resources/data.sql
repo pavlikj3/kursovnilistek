91bdfa296d124f46dee99504bc65238e8af8d57d
Jan Pavlik <pavlikj3@seznam.cz>
2021-03-07 18:29:11 +0100

    Prejmenovani test.html na index.html

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into kursovnilistek (currency_full_name, cash_buy, cash_sell) values ('Česká koruna', 1.05, 0.95);
+insert into kursovnilistek (currency_full_name, cash_buy, cash_sell) values ('Česká koruna', 0.95, 1.05);
 
-insert into kursovnilistek (currency_full_name, cash_buy, cash_sell) values ('Rubl', 0.35, 0.30);
+insert into kursovnilistek (currency_full_name, cash_buy, cash_sell) values ('Rubl', 0.30, 0.35);
