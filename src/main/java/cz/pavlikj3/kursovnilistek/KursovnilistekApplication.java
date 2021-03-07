40e824f66572ed3698e63de120368ed086f08496
Jan Pavlik <pavlikj3@seznam.cz>
2021-03-07 11:03:35 +0100

    Inicializacni import java projektu

---
+++ src/main/java/cz/pavlikj3/kursovnilistek/KursovnilistekApplication.java
@@ -0,0 +1,13 @@
+package cz.pavlikj3.kursovnilistek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursovnilistekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KursovnilistekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/pavlikj3/kursovnilistek/KursovnilistekApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.pavlikj3.kursovnilistek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KursovnilistekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
